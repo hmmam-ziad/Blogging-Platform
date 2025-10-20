b510960581462bd0e311e6791bb98f444a8f2c7c
hmmam-ziad <hmmamxxc@gmail.com>
2025-10-20 22:38:30 +0300

    Comment Controller

---
+++ Blogging-Platform-Backend/Blogging-Platform-Backend/Controllers/CommentsController.cs
@@ -0,0 +1,66 @@
+﻿using Blogging_Platform_Backend.Data;
+using Blogging_Platform_Backend.DTOModels.Comments;
+using Blogging_Platform_Backend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace Blogging_Platform_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CommentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // we gonna implement GetCommentsByPost/{postId} if we need later.
+        [Authorize]
+        [HttpPost("[action]")]
+        public async Task<IActionResult> AddComment(dtoComment model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                var postExists = await _context.Posts.AnyAsync(p => p.PostId == model.PostId);
+                if (!postExists) 
+                    return NotFound();  
+                var comment = new Comment
+                {
+                    Content = model.Content,
+                    UserId = userId!,
+                    PostId = model.PostId,
+                    CreatedAt = DateTime.UtcNow,
+                };
+
+                _context.Comments.Add(comment);
+                await _context.SaveChangesAsync();
+                return Ok(new { message = "Your comment was added successfully" });
+            }
+            return BadRequest(ModelState);
+        }
+
+        [Authorize]
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> DeleteComment(string Id)
+        {
+            var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var comment = await _context.Comments.FirstOrDefaultAsync(c => c.CommentId == Id);
+            if (comment == null)
+                return NotFound();
+
+            if (comment.UserId != currentUserId)
+                return Forbid();
+
+            _context.Comments.Remove(comment);
+            await _context.SaveChangesAsync();
+            return Ok(new {message = "Your comment was Deleted successfully" });
+        }
+    }
+}
---
+++ Blogging-Platform-Backend/Blogging-Platform-Backend/DTOModels/Comments/dtoComment.cs
@@ -0,0 +1,12 @@
+﻿using Blogging_Platform_Backend.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blogging_Platform_Backend.DTOModels.Comments
+{
+    public class dtoComment
+    {
+        public string Content { get; set; }
+        public string PostId { get; set; }
+    }
+}
